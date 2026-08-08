b3655a9b0d59c8103f5ede7ddc47dbc4e8a37d6f
Owen Lusis <owen00064@gmail.com>
2026-08-08 19:10:24 +1000

    use go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const App());
+    runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: Text("Journal")),
-            body: Center(
-                child: Stack(
+final _router = GoRouter(
+    routes: [
+        GoRoute(
+            path: "/",
+            builder: (context, state) => Scaffold(
+                appBar: AppBar(title: Text("Home Screen")),
+                body: Center(
+                    child: Text("Home"),
                 )
             )
-        ),
-    );
-  }
+        )
+    ]
+);
+
+class App extends StatelessWidget {
+    const App({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp.router(
+            routerConfig: _router,
+        );
+    }
 }
 
