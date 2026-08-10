2603c7f4391f0808d3168af431e2c1797190e0b3
Owen Lusis <owen00064@gmail.com>
2026-08-10 16:49:11 +1000

    move entry list to separate component

---
+++ lib/widgets/entry_list.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:journal/db.dart';
+import 'package:journal/widgets/entry_card.dart';
+
+class EntryList extends ConsumerWidget {
+  const EntryList({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final entries = ref.watch(itemsProvider);
+
+    return switch (entries) {
+      AsyncData(:final value) => ListView.builder(
+        itemCount: value.length,
+        itemBuilder: (context, index) => EntryCard(entry: value[index]),
+      ),
+      AsyncError(:final error) => Center(child: Text(error.toString())),
+      _ => Center(child: const CircularProgressIndicator()),
+    };
+  }
+}
--- lib/widgets/home_screen.dart
+++ lib/widgets/home_screen.dart
@@ -1,28 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:journal/db.dart';
-import 'package:journal/widgets/entry_card.dart';
+import 'package:journal/widgets/entry_list.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final entries = ref.watch(itemsProvider);
-
-    return switch (entries) {
-      AsyncData(:final value) => Scaffold(
-        appBar: AppBar(title: Text("Bar Title")),
-        body: ListView.builder(
-          itemCount: value.length,
-          itemBuilder: (context, index) => EntryCard(entry: value[index]),
-        ),
-      ),
-      AsyncError(:final error) => Scaffold(
-        appBar: AppBar(title: Text("Error")),
-        body: Text(error.toString()),
-      ),
-      _ => const CircularProgressIndicator(),
-    };
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Journal")),
+      body: EntryList(),
+    );
   }
 }
