1d918c02eb60f5849e2e84b0a62854d2df0392d8
Owen Lusis <owen00064@gmail.com>
2026-08-14 09:49:26 +1000

    add dividers

--- lib/widgets/entry_card.dart
+++ lib/widgets/entry_card.dart
@@ -29,6 +29,7 @@ class EntryCard extends StatelessWidget {
                 color: Theme.of(context).colorScheme.primary,
               ),
             ),
+            Divider(),
           ],
         ),
       ),
