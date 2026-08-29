dd630e38b70beb1b2eeaca61016ec3c4ffbc63ab
Owen Lusis <owen00064@gmail.com>
2026-08-29 16:47:34 +1000

    add .debug suffix to application id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,6 +39,9 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+        debug {
+            applicationIdSuffix = ".debug"
+        }
     }
 }
 
